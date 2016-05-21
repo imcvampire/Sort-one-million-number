04e3661d0375848963a0d5a47a3f3c732df62130
imcvampire <quocanh96@gmail.com>
2016-05-22 00:12:12 +0700

    Rewrite merge. Memory usage > 3MB

--- sort.cpp
+++ sort.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 #define n 1000000 // Consumes 4MB
 #define m 500000  // Consumes 2MB
-#define p 250000
+#define p 100000
 
 int main()
 {
@@ -47,102 +47,7 @@ int main()
 
 	int temp[p];
 
-	// First 250000 number in temp file
-	for (int i = 0; i < p; ++i)
-	{
-		fs >> temp[i];
-	}		
 	
-	int i = 0, // index in temp
-		j = 0; // index in v
-	bool flag = true;
-		
-	while (i < p && j < m)
-	{
-		if (temp[i] > v[j])
-		{
-			// output << v[j] << endl;
-			cout << v[j] << endl;
-			++j;
-		}
-		else 
-		{
-			// output << temp[i] << endl;
-			cout << temp[i] << endl;
-			++i;
-		}
-		
-		// Check j are in range
-		if (j == m)
-		{
-			// Print others number in temp array
-			for (int k = j; k < p; ++k)
-			{
-				// output << temp[i] << endl;
-				cout << temp[i] << endl;
-			}
-			
-			//Print others number in temp file
-			for (int k = 0; k < p; ++k)
-			{
-				fs >> temp[k]; 
-			}
-			
-			for (int k = 0; k < p; ++k)
-			{
-				// output << temp[k] << endl;
-				cout << temp[k] << endl;
-			}
-			
-			flag = false;
-		}
-	}
-	
-	// Next 250000 number in temp file
-	if (flag)
-	{
-		for (int k = 0; k < p; ++k)
-		{
-			fs >> temp[k]; 
-		}
-		
-		i = 0;
-		
-		while (i < p && j < m)
-		{
-			if (temp[i] > v[j])
-			{
-				// output << v[j] << endl;
-				cout << v[j] << endl;
-				++j;
-			}
-			else 
-			{
-				// output << temp[i] << endl;
-				cout << temp[i] << endl;
-				++i;
-			}
-			
-			// Check i and j in range
-			if (i == p)
-			{
-				for (int k = j; k < m; ++k)
-				{
-					// output << v[k] << endl;
-					cout << v[k] << endl;
-				}
-			}
-			
-			if (j == m)
-			{
-				for (int k = i; k < p; ++k)
-				{
-					// output << temp[k] << endl;
-					cout << temp[k] << endl;
-				}
-			}
-		}
-	}
 	
 	fs.close();
 	// output.close();
