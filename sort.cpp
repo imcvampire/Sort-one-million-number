46a7bcd1cb5b8ace4a89b88c76832c5dd6d85b15
imcvampire <quocanh96@gmail.com>
2016-06-04 21:13:35 +0700

    fix 2-way merge

--- sort.cpp
+++ sort.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 #define n 1000000 // Consumes 4MB
 #define m 500000  // Consumes 2MB
-#define p 250000
 
 int main()
-{
-	
+{	
 	int v[m];
 	
 	// Read first 500000 number
@@ -41,95 +39,53 @@ int main()
 		
 	// 2-way merge
 	fstream fs ("temp", fstream::in);
-
-	int temp[p];
-
-	// First 250000 number in temp file
-	for (int i = 0; i < p; ++i)
-	{
-		fs >> temp[i];
-	}		
 	
-	int i = 0, // index in temp
-		j = 0; // index in v
-	bool flag = true;
-		
-	while (i < p && j < m)
+	int i_temp;
+	fs >> i_temp;
+	
+	int i = 0,	// index in v
+		j = 0;	// index in temp
+	int result = 0;
+	
+	while (i < m)
 	{
-		if (temp[i] > v[j])
+		if (v[i] > i_temp)
 		{
-			cout << v[j] << endl;
+			cout << i_temp << endl;
+			fs >> i_temp;
 			++j;
 		}
-		else 
+		else
 		{
-			cout << temp[i] << endl;
+			cout << v[i] << endl;
 			++i;
 		}
 		
-		// Check j are in range
+		if (i == m) 
+		{
+			result = 1;
+		}
+		
 		if (j == m)
 		{
-			// Print others number in temp array
-			for (int k = j; k < p; ++k)
-			{
-				cout << temp[i] << endl;
-			}
-			
-			//Print others number in temp file
-			for (int k = 0; k < p; ++k)
-			{
-				fs >> temp[k]; 
-			}
-			
-			for (int k = 0; k < p; ++k)
-			{
-				cout << temp[k] << endl;
-			}
-			
-			flag = false;
+			result = -1;
+			break;
 		}
 	}
 	
-	// Next 250000 number in temp file
-	if (flag)
+	if (result == 1)
 	{
-		for (int k = 0; k < p; ++k)
+		for (; j < m; ++j)
 		{
-			fs >> temp[k]; 
+			fs >> i_temp;
+			cout << i_temp << endl;
 		}
-		
-		i = 0;
-		
-		while (i < p && j < m)
+	} 
+	else if (result == -1)
+	{
+		for (; i < m; ++i)
 		{
-			if (temp[i] > v[j])
-			{
-				cout << v[j] << endl;
-				++j;
-			}
-			else 
-			{
-				cout << temp[i] << endl;
-				++i;
-			}
-			
-			// Check i and j in range
-			if (i == p)
-			{
-				for (int k = j; k < m; ++k)
-				{
-					cout << v[k] << endl;
-				}
-			}
-			
-			if (j == m)
-			{
-				for (int k = i; k < p; ++k)
-				{
-					cout << temp[k] << endl;
-				}
-			}
+			cout << v[i] << endl;
 		}
 	}
 	
